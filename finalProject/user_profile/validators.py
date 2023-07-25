75806445a35ebe2fff70e9cd5e0dc0c4b968cb9c
YoanLigov2 <140204865+YoanLigov2@users.noreply.github.com>
2023-07-25 19:00:58 +0300

    Delete finalProject directory

--- finalProject/user_profile/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.validators import ValidationError
-
-
-def only_alphabetical_letters(value):
-    if not value.isalpha():
-        raise ValidationError("Must contain only alphabetical letters!!!")
