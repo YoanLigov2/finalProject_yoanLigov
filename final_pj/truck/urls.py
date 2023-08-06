363b4f5d758d58627811ad61429a52e253ea34d3
YoanLigov2 <140204865+YoanLigov2@users.noreply.github.com>
2023-08-06 21:36:51 +0300

    Delete final_pj directory

--- final_pj/truck/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import truck_create, TruckDetailsView, TruckEditView, TruckDeleteView
-
-urlpatterns = [
-    path('create/', truck_create, name='truck-create'),
-    path('<int:pk>/details/', TruckDetailsView.as_view(), name='truck-details'),
-    path('<int:pk>/edit/', TruckEditView.as_view(), name='truck-edit'),
-    path('<int:pk>/delete/', TruckDeleteView.as_view(), name='truck-delete'),
-]
