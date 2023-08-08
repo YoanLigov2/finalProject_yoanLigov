7e040a0a288c32a426dba76fed0effaeac669e31
YoanLigov2 <140204865+YoanLigov2@users.noreply.github.com>
2023-08-08 15:37:15 +0300

    Add files via upload

---
+++ final_pj/yacht/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import Yacht
+
+
+class YachtForm(forms.ModelForm):
+    class Meta:
+        model = Yacht
+        exclude = ('user', )
+        labels = {
+            'yacht_length': "Yacht Length(in meters)",
+            'very_short_description': 'Very Short Description(Optional)',
+            'price': 'Price in Bulgarian leva',
+        }
+
+
+class YachtDeleteForm(YachtForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__set_disabled_fields()
+
+    def save(self, commit=True):
+        if commit:
+            self.instance.delete()
+        return self.instance
+
+    def __set_disabled_fields(self):
+        for field in self.fields.values():
+            field.widget.attrs['disabled'] = 'disabled'
+            field.required = False
