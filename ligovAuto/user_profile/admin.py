84a84b193ca29200c25479bcdae450243dbcc381
YoanLigov2 <140204865+YoanLigov2@users.noreply.github.com>
2023-08-12 17:10:22 +0300

    Update admin.py

--- ligovAuto/user_profile/admin.py
+++ ligovAuto/user_profile/admin.py
@@ -8,11 +8,4 @@ class CollectionUserAdmin(admin.ModelAdmin):
     list_filter = ['username', 'first_name', 'last_name']
     ordering = ['-username']
     search_fields = ['username', 'first_name', 'last_name']
-    fieldsets = (
-        ('Personal info',
-         {'fields': ('username', 'first_name', 'last_name', 'gender', 'password')}),
-        ('Contacts',
-         {'fields': ('phone_number', 'email'), }),
-    )
-
-
+    
